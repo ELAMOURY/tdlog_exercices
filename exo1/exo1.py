9502e0428d9b639d5fcc672575ac7053c323f3e3
ELAMOURY <148467000+ELAMOURY@users.noreply.github.com>
2024-09-26 09:35:29 +0100

    Premier commit

--- exo1/exo1.py
+++ exo1/exo1.py
@@ -7,3 +7,16 @@ A new item can be created with Item(price, weight)
     Write the code for this class, with the appropriate constructor.
 Example of code using the class: i = Item(10, 20)
 """
+class Item:
+    def __init__(self, price, weight):
+        # Constructeur : initialise les attributs de prix et de poids
+        self.price = price
+        self.weight = weight
+    
+    def afficher_info(self):
+        # Méthode pour afficher les informations de l'objet
+        print(f"Prix : {self.price}, Poids : {self.weight}")
+
+# Exemple d'utilisation de la classe
+i = Item(10, 20)
+i.afficher_info()  # Affiche : Prix : 10, Poids : 20
